dd9db469fbd5cbd85e1bc5889f3cae41c9e2ec77
liugh <1509444199@qq.com>
2023-02-12 07:50:13 +0800

    liugh:添加内核线程demo

---
+++ kernel_thread/thread.c
@@ -0,0 +1,58 @@
+#include <linux/delay.h>
+#include <linux/err.h>
+#include <linux/kernel.h>
+#include <linux/kthread.h>
+#include <linux/module.h>
+#include <linux/sched.h>
+
+static struct stask_struct *test_task_struct;
+
+int thread_task(void *arg)
+{
+	static int count = 0;
+	while(1){
+		if(kthread_should_stop()){
+			printk("thread task: kthread_should_stop\n");
+			break;
+		}
+
+		printk("thread task: count=%d\n",++count);
+
+		ssleep(1);
+	}
+
+	return 0;
+}
+
+
+static int __init init_kernel_thread(void)
+{
+	test_task_struct = kthread_create(thread_task, NULL, "Kernel Thread");
+    if(IS_ERR(test_task_struct)){
+		printk("kthread_create error\n");
+	}else{
+		wake_up_process(test_task_struct);
+	}
+	return 0;
+}
+
+
+static void __exit exit_kernel_thread(void)
+{
+	kthread_stop(test_task_struct);
+}
+
+module_init(init_kernel_thread);
+module_exit(exit_kernel_thread);
+
+
+
+
+
+
+
+
+
+
+
+
