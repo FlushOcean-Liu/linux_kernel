93ae4da3712def95fed64c538827724dd2839b6b
liugh <1509444199@qq.com>
2023-03-09 17:28:54 +0800

    添加打补丁命令示例

---
+++ patch/patch.cmd
@@ -0,0 +1,11 @@
+diff -uprN linux-0.11/ linux-0.11_new  > linux.patch
+
+# 生成补丁
+diff -uprN raw.txt new.txt  > linux.patch
+
+# 打补丁，在原版目录下执行打补丁命令，-p后面代表忽略哪一级路径
+patch -p0 < linux.patch
+
+# 卸载补丁，还原打补丁之前的状态
+patch -p0 -R < linux.patch
+
